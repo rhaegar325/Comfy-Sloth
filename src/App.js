ea94bc704b184861b62d7da404358d28a4614417
rhaegar325 <u6660070@anu.edu.au>
2022-11-08 15:12:35 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+
+import styled from 'styled-components'
+
+import {Home, Products,SingleProduct,About,Cart,Error,Checkout,PrivateRoute, AuthWrapper} from "./pages"
+
+
+function App() {
+  return (
+    <AuthWrapper>
+      <Router>
+        <Navbar></Navbar>
+        <Sidebar></Sidebar>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/about'>
+            <About></About>
+          </Route>
+          <Route exact path='/cart'>
+            <Cart></Cart>
+          </Route>
+          <Route exact path='/products'>
+            <Products></Products>
+          </Route>
+          <Route exact path='/products/:id' children={<SingleProduct></SingleProduct>}>
+            
+          </Route>
+          <PrivateRoute exact path='/checkout'>
+            <Checkout></Checkout>
+          </PrivateRoute>
+          <Route exact path='*'>
+            <Error></Error>
+          </Route> 
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </AuthWrapper>)
+  
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import About from "./AboutPage";
+import Home from "./HomePage"
+import AuthWrapper from "./AuthWrapper"
+import Cart from "./CartPage"
+import Checkout from "./CheckoutPage"
+import Error from "./ErrorPage"
+import PrivateRoute from "./PrivateRoute"
+import Products from "./ProductsPage"
+import SingleProduct from "./SingleProductPage"
+
+
+export {Home, Products,SingleProduct,About,Cart,Error,Checkout,PrivateRoute,AuthWrapper}
